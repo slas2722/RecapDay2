df14498aa9949c80403fdd69a78759ef04938674
slas2 <slas2@DESKTOP-1PALHLO>
2023-07-30 10:09:49 -0400

    implement sub

--- src/Recap/RecapDemo.java
+++ src/Recap/RecapDemo.java
@@ -1,14 +1,18 @@
 package Recap;
 
 public class RecapDemo {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Recap");
 	}
+
 	public void subtract(int a, int b) {
 		System.out.println(a - b);
 	}
 
+	public void add(int a, int b) {
+		System.out.println(a + b);
+	}
 
 }
